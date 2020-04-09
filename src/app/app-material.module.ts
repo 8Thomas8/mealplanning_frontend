fd9642f095695c639a2de8b9c37ed8c6e23e37eb
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-04-09 11:02:46 +0200

    Ajout de la barre de navigation

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,9 +1,14 @@
-import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-const importExport = [MatButtonModule]
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+
+const importExport = [MatButtonModule, MatToolbarModule, MatIconModule];
 
 @NgModule({
   imports: importExport,
   exports: importExport
 })
-export class AppMaterialModule { }
+export class AppMaterialModule {
+}
