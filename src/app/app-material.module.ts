a9fc34ac751d650946f9acdc423cf7fedc691db9
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-04-09 11:27:25 +0200

    Add tooltip

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 
-const importExport = [MatButtonModule, MatToolbarModule, MatIconModule];
+const importExport = [MatButtonModule, MatToolbarModule, MatIconModule, MatTooltipModule];
 
 @NgModule({
   imports: importExport,
