86d1ff2b2940d954090814c234b63427b367f3f8
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-04-15 23:50:32 +0200

    Correction du service d'authentication

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,35 +1,43 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
-import {User} from '../../models/user';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import {environment} from '../../../environments/environment';
+import {CurrentUser} from '../../models/current-user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  private currentUserSubject: BehaviorSubject<User>;
-  public currentUser: Observable<User>;
+  private currentUserSubject: BehaviorSubject<CurrentUser>;
+  public currentUser: Observable<CurrentUser>;
 
   constructor(private http: HttpClient) {
-    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUserSubject = new BehaviorSubject<CurrentUser>(JSON.parse(localStorage.getItem('currentUser')));
     this.currentUser = this.currentUserSubject.asObservable();
   }
 
-  public get currentUserValue(): User {
+  public get currentUserValue(): CurrentUser {
     return this.currentUserSubject.value;
   }
 
-  login(name: string, password: string) {
-    return this.http.post<any>(`${environment.apiUrl}/user/authenticate`, {name, password}).pipe(map(user => {
-      if (user) {
-        localStorage.setItem('currentUser', JSON.stringify(user));
-        this.currentUserSubject.next(user);
-      }
+  login(username: string, password: string) {
+    const body = new URLSearchParams();
+    body.set('username', username);
+    body.set('password', password);
+    const options = {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
 
-      return user;
-    }));
+    return this.http.post(`${environment.apiUrl}/login`, body.toString(), options);
+  }
+
+  getLogged() {
+    return this.http.get<CurrentUser>(`${environment.apiUrl}/getLogged`).pipe(map(currentUser => {
+        this.currentUserSubject.next(currentUser);
+        localStorage.setItem('currentUser', JSON.stringify(currentUser));
+      }
+    ));
   }
 
   logout() {
