d23cb893ea48659a54c62b28e084ce4c1485cadb
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-04-22 12:56:43 +0200

    Modification de la gestion csrf au logout et à l'arrivée sur la page de login

--- src/app/commons/nav/nav.component.ts
+++ src/app/commons/nav/nav.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from '../../services/authentication/authentication.service';
 import {CurrentUser} from '../../models/current-user';
 import {Router} from '@angular/router';
+import {CsrfService} from '../../services/csrf/csrf.service';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,8 @@ export class NavComponent implements OnInit {
 
   currentUser: CurrentUser;
 
-  constructor(private authenticationService: AuthenticationService, private router: Router) { }
+  constructor(private authenticationService: AuthenticationService, private router: Router, private csrfService: CsrfService) {
+  }
 
   ngOnInit(): void {
     this.getCurrentUser();
@@ -22,10 +24,12 @@ export class NavComponent implements OnInit {
     this.authenticationService.currentUser.subscribe(currentUser => this.currentUser = currentUser);
 
   }
+
   onLogout() {
     const logout = this.authenticationService.logout().toPromise();
 
     logout.then(() => {
+      this.csrfService.csrfStatusToFalse();
       this.router.navigateByUrl('/login');
     });
   }
