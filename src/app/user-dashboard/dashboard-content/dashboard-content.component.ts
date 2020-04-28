b67dbca80742a1de7c941c5a74d9bd20b36950f6
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-04-28 18:22:29 +0200

    Modification dashboard content

--- src/app/user-dashboard/dashboard-content/dashboard-content.component.ts
+++ src/app/user-dashboard/dashboard-content/dashboard-content.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CreatePlanningModalService} from '../../services/create-planning-modal.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ApiPlanningService} from '../../services/api/api-planning.service';
+import {Planning} from '../../models/planning';
+import {PlanningService} from '../../services/planning.service';
+
+const ERROR_EMPTY = 'Vous devez entrer une valeur';
 
 @Component({
   selector: 'app-dashboard-content',
@@ -6,10 +13,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-content.component.sass']
 })
 export class DashboardContentComponent implements OnInit {
+  showCreateModalStatus: boolean;
 
-  constructor() { }
+  newPlanningForm = new FormGroup({
+    name: new FormControl('', [Validators.required])
+  });
 
-  ngOnInit(): void {
+  constructor(private createPlanningModalService: CreatePlanningModalService, private planningService: PlanningService) {
   }
 
+  ngOnInit(): void {
+    this.getCreateModalStatus();
+  }
+
+  getCreateModalStatus() {
+    this.createPlanningModalService.getStatus().subscribe(status => this.showCreateModalStatus = status);
+  }
+
+  getNameErrorMessage() {
+    if (this.newPlanningForm.get('name').hasError('required')) {
+      return ERROR_EMPTY;
+    }
+  }
+
+  onCreate() {
+    if (this.newPlanningForm.invalid) {
+      return;
+    }
+
+    const planning = new Planning();
+    planning.name = this.newPlanningForm.get('name').value;
+
+    this.planningService.addPlanning(planning);
+  }
+
+  onClose() {
+    this.newPlanningForm.reset();
+    this.createPlanningModalService.setFalse();
+  }
 }
